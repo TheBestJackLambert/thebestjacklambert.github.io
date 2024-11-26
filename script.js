d727425fece3c0313f59ac74a6a9d88899c1291e
TheBestJackLambert <153460815+TheBestJackLambert@users.noreply.github.com>
2024-11-26 17:28:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ hoverElements.forEach(elem => {
 
 // Typed.js Initialization
 var typed = new Typed('#typed', {
-  strings: ['Physics Enthusiast', 'Aspiring Engineer', 'Problem Solver'],
+  strings: ['Physics Enthusiast', 'Chess Lover', 'Problem Solver', 'Awesome dude'],
   typeSpeed: 50,
   backSpeed: 50,
   loop: true
